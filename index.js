24ced07ab1f018c016b6256a213397c35397d52a
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-01-02 16:14:44 -0300

    Inserção de funcionalidades - CRUD

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 
 const User = require('./models/User')
+const Address = require('./models/Address')
 
 const app = express()
 
@@ -56,8 +57,59 @@ app.post('/users/delete/:id', async (req, res) => {
 app.get('/users/edit/:id', async (req, res) => {
     const id = req.params.id
 
-    const user = await User.findOne({ raw: true, where: { id: id } })
-    res.render('useredit', { user })
+    const user = await User.findOne({ include: Address, where: { id: id } })
+    res.render('useredit', { user: user.get({ plain: true }) })
+})
+
+app.post('/users/update', async (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if (newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    const userData = {
+        id,
+        name,
+        occupation,
+        newsletter
+    }
+
+    await User.update(userData, { where: { id: id } })
+    res.redirect('/')
+})
+
+app.post('/address/create', async (req, res) => {
+    const UserId = req.body.UserId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const address = {
+        UserId,
+        street,
+        number,
+        city,
+    }
+
+    await Address.create(address)
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+app.post('/address/delete', async (req, res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+
+    await Address.destroy({
+        where: { id: id }
+    })
+
+    res.redirect(`/users/edit/${UserId}`)
 })
 
 app.get('/', async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require('sequelize')
-
 const db = require('../db/conn')
 
 const User = db.define('User', {
